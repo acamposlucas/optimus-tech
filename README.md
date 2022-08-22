791575448b55899054dcd677cb3b8f15746d2191
lucas <lac.lucas96@gmail.com>
2022-08-22 14:38:42 -0300

    feat: add js to toggle navbar. add readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# OptimusTech
+
+This is a landing page created with TailwindCSS for a fictional company called OptimusTech. It is a project to practice HTML and CSS made by Alura on its 7 Days of Code series. You can participate ![here](https://7daysofcode.io/matricula/html-css).
+
+## Technologies and methods
+
+- HTML
+- CSS
+- JavaScript
+- TailwindCSS
---
+++ main.js
@@ -0,0 +1,15 @@
+const NAV_BUTTON = document.getElementById('nav-button');
+const MAIN_NAVIGATION = document.getElementById('main-navigation');
+const LAPTOP_VIEWPORT_WIDTH = 1024;
+
+NAV_BUTTON.addEventListener('click', () => {
+  MAIN_NAVIGATION.classList.toggle('hidden')
+});
+
+window.addEventListener('resize', () => {
+  let viewportWidth = window.innerWidth;
+  
+  if (viewportWidth >= LAPTOP_VIEWPORT_WIDTH) {
+    MAIN_NAVIGATION.classList.remove('hidden');
+  }
+});
