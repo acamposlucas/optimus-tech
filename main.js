340e082385d61fab1b25f1311a81a4aa7b85a2a4
lucas <lac.lucas96@gmail.com>
2022-08-27 11:57:29 -0300

    feat: add close icon when nav is open

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const NAV_BUTTON = document.getElementById('nav-button');
 const MAIN_NAVIGATION = document.getElementById('main-navigation');
 const LAPTOP_VIEWPORT_WIDTH = 1024;
+let isNavigationOpen = false;
 
 NAV_BUTTON.addEventListener('click', () => {
-  MAIN_NAVIGATION.classList.toggle('hidden')
+  MAIN_NAVIGATION.classList.toggle('hidden');
+  isNavigationOpen = !isNavigationOpen;
+  NAV_BUTTON.setAttribute("aria-expanded", isNavigationOpen);
+  MAIN_NAVIGATION.setAttribute("aria-expanded", isNavigationOpen)
 });
 
 window.addEventListener('resize', () => {
   let viewportWidth = window.innerWidth;
-  
+
   if (viewportWidth >= LAPTOP_VIEWPORT_WIDTH) {
     MAIN_NAVIGATION.classList.remove('hidden');
   }
